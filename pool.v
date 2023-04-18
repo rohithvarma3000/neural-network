8434cbde84a2fa2ae3b4992dc6a213746cd0d14b
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2023-04-18 23:56:09 +0530

    Update pool.v

--- pool.v
+++ pool.v
@@ -1,8 +1,9 @@
 `timescale 1ns / 1ps
 
-module compare(input [INTEGER_BITS+FIXED_POINT_BITS-1:0] a, input [INTEGER_BITS+FIXED_POINT_BITS-1:0] b, output reg [INTEGER_BITS+FIXED_POINT_BITS-1:0] c);
-    parameter INTEGER_BITS = 9;
-    parameter FIXED_POINT_BITS = 4;
+module compare #(
+    parameter INTEGER_BITS = 9,
+    parameter FIXED_POINT_BITS = 4
+)(input [INTEGER_BITS+FIXED_POINT_BITS-1:0] a, input [INTEGER_BITS+FIXED_POINT_BITS-1:0] b, output reg [INTEGER_BITS+FIXED_POINT_BITS-1:0] c);
 
     always @(a,b) begin
         if(a[INTEGER_BITS+FIXED_POINT_BITS-1]==0 && b[INTEGER_BITS+FIXED_POINT_BITS-1] == 0) c = (a>b)? a:b;
@@ -12,9 +13,10 @@ module compare(input [INTEGER_BITS+FIXED_POINT_BITS-1:0] a, input [INTEGER_BITS+
     end
 endmodule
 
-module max_pool_2 (input clk, input [(INTEGER_BITS+FIXED_POINT_BITS)*4-1:0] data, output reg [INTEGER_BITS+FIXED_POINT_BITS-1:0] out);
-    parameter INTEGER_BITS = 9;
-    parameter FIXED_POINT_BITS = 4;
+module max_pool_2 #(
+    parameter INTEGER_BITS = 9,
+    parameter FIXED_POINT_BITS = 4
+) (input clk, input [(INTEGER_BITS+FIXED_POINT_BITS)*4-1:0] data, output reg [INTEGER_BITS+FIXED_POINT_BITS-1:0] out);
     reg [INTEGER_BITS+FIXED_POINT_BITS-1:0] pool[3:0];
 
     reg [INTEGER_BITS+FIXED_POINT_BITS-1:0] comp_in1, comp_in2, max;
