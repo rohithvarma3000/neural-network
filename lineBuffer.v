1f74d4d1c5d1207fd0d8ac9061611377fbee3e85
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2023-04-18 23:54:40 +0530

    Update lineBuffer.v

--- lineBuffer.v
+++ lineBuffer.v
@@ -1,17 +1,16 @@
 `timescale 1ns / 1ps
 
-
-
-module lineBuffer(
+module lineBuffer #(
+    parameter INTEGER_BITS = 9,
+    parameter FIXED_POINT_BITS = 4
+)(
 input   i_clk,
 input   i_rst,
 input [INTEGER_BITS+FIXED_POINT_BITS-1:0] i_data,
 input   i_data_valid,
 output [(INTEGER_BITS+FIXED_POINT_BITS)*3-1:0] o_data,
-input i_rd_data,
+input i_rd_data
 );
-parameter INTEGER_BITS = 9;
-parameter FIXED_POINT_BITS = 4;
 
 reg [INTEGER_BITS+FIXED_POINT_BITS-1:0] line [511:0]; //line buffer
 reg [8:0] wrPntr;
