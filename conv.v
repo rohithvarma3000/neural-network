3156a5a64e1371fcf9e27e8577b0fc905832b2b5
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2023-04-18 23:52:45 +0530

    Update conv.v

--- conv.v
+++ conv.v
@@ -1,7 +1,9 @@
 `timescale 1ns / 1ps
 
-
-module conv(
+module conv #(
+    parameter INTEGER_BITS = 9,
+    parameter FIXED_POINT_BITS = 4
+)(
 input        i_clk,
 input [(INTEGER_BITS+FIXED_POINT_BITS)*9-1:0] i_pixel_data,
 input        i_pixel_data_valid,
@@ -11,10 +13,7 @@ output reg [INTEGER_BITS+FIXED_POINT_BITS-1:0] o_convolved_data,
 output reg   o_convolved_data_valid
     );
 
-    parameter INTEGER_BITS = 8;
-parameter FIXED_POINT_BITS = 4;
-    
-integer i; 
+
 reg [INTEGER_BITS+FIXED_POINT_BITS-1:0] kernel [8:0];
 reg [(INTEGER_BITS+FIXED_POINT_BITS)*2-1:0] multData[8:0];
 reg [(INTEGER_BITS+FIXED_POINT_BITS)*2-1:0] sumDataInt;
@@ -24,6 +23,7 @@ reg [(INTEGER_BITS+FIXED_POINT_BITS)*2-1:0] temp2;
 reg multDataValid;
 reg sumDataValid;
 reg convolved_data_valid;
+reg[3:0] i;
 
 always @(posedge kernel_reset) begin
     kernel[0] = kernel_vals[(INTEGER_BITS+FIXED_POINT_BITS)*9-1:(INTEGER_BITS+FIXED_POINT_BITS)*8];
@@ -39,10 +39,16 @@ end
     
 always @(posedge i_clk)
 begin
-    for(i=0;i<9;i=i+1)
-    begin
-        multData[i] <= kernel[i]*i_pixel_data[i*(INTEGER_BITS+FIXED_POINT_BITS):(i+1)*(INTEGER_BITS+FIXED_POINT_BITS)];
-    end
+    multData[0] = kernel[0]*i_pixel_data[(INTEGER_BITS+FIXED_POINT_BITS)-1:0];
+    multData[1] = kernel[1]*i_pixel_data[2*(INTEGER_BITS+FIXED_POINT_BITS)-1:1*(INTEGER_BITS+FIXED_POINT_BITS)];
+    multData[2] = kernel[2]*i_pixel_data[3*(INTEGER_BITS+FIXED_POINT_BITS)-1:2*(INTEGER_BITS+FIXED_POINT_BITS)];
+    multData[3] = kernel[3]*i_pixel_data[4*(INTEGER_BITS+FIXED_POINT_BITS)-1:3*(INTEGER_BITS+FIXED_POINT_BITS)];
+    multData[4] = kernel[4]*i_pixel_data[5*(INTEGER_BITS+FIXED_POINT_BITS)-1:4*(INTEGER_BITS+FIXED_POINT_BITS)];
+    multData[5] = kernel[5]*i_pixel_data[6*(INTEGER_BITS+FIXED_POINT_BITS)-1:5*(INTEGER_BITS+FIXED_POINT_BITS)];
+    multData[6] = kernel[6]*i_pixel_data[7*(INTEGER_BITS+FIXED_POINT_BITS)-1:6*(INTEGER_BITS+FIXED_POINT_BITS)];
+    multData[7] = kernel[7]*i_pixel_data[8*(INTEGER_BITS+FIXED_POINT_BITS)-1:7*(INTEGER_BITS+FIXED_POINT_BITS)];
+    multData[8] = kernel[8]*i_pixel_data[9*(INTEGER_BITS+FIXED_POINT_BITS)-1:8*(INTEGER_BITS+FIXED_POINT_BITS)];
+    
     multDataValid <= i_pixel_data_valid;
 end
 
